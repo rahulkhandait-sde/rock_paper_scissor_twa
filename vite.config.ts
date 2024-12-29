54ff77c656d55cfbdd11eb140b7b80b129e73487
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2024-12-29 16:36:24 +0530

    file updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      buffer: 'buffer/',
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer'],
-  },
   plugins: [react()],
 })
